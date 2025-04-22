de68884c5d3a8a752c538020cc14e8450b17d7ee
Pushkar <chavhanpushkar111@gmail.com>
2025-04-22 10:15:10 +0530

    mern stack project

---
+++ crud-application/client/src/App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+import AddUser from "./addUser/AddUser";
+
+import User from "./getuser/User";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Update from "./updateuser/Update";
+
+function App() {
+  const route = createBrowserRouter([
+    {
+      path: "/",
+      element: <User />,
+    },
+    {
+      path: "/add",
+      element: <AddUser />,
+    },
+    {
+      path: "/update/:id",
+      element: <Update />,
+    },
+  ]);
+  return (
+    <div className="App">
+      <RouterProvider router={route}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
